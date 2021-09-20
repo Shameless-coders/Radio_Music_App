578e8f51b358e5e221fab63d12533bc4d78b3ca0
tingwei xie <tix980@mail.usask.ca>
2021-09-20 14:09:55 -0400

    add hello world text

--- App.js
+++ App.js
@@ -1,17 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
     return (
-        <NavigationContainer>
             <View style={styles.container}>
-                <Text>Open up App.js to start working on your app!</Text>
+                <Text>Hello World!</Text>
                 <StatusBar style="auto" />
             </View>
-        </NavigationContainer>
   );
 }
 
