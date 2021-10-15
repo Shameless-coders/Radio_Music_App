55d018f7097320684d918c31ce07ccc8370da107
Natasha Chambers <natashaagathachambers@gmail.com>
2021-10-15 13:47:40 -0400

    making changes to music and home screen

--- components/screens/main/homescreen.js
+++ components/screens/main/homescreen.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from 'react-native';
 import { Card } from "react-native-elements";
+import LinearGradient from "react-native-linear-gradient";
 
 class Home extends Component {
     render() {
         return (
-            <View>
-              <Card>
+            <View style={styles.container}>
+              <Card containerStyle={styles.card}>
                 <Card.Image source={require('../../../assets/images/logo-2.png')}></Card.Image>
-                <Card.Title>Hello World!</Card.Title>
                 <Card.Divider />
-                <Card.FeaturedSubtitle>Listen to all your favourite music!</Card.FeaturedSubtitle>
-            </Card>
+                <Card.Title>Hello World!</Card.Title>
+              </Card>
             </View>
         );
     }
@@ -19,11 +19,18 @@ class Home extends Component {
 
 export default Home;
 
-// const styles = StyleSheet.create({
-//     container: {
-//       flex: 1,
-//       backgroundColor: '#fff',
-//       alignItems: 'center',
-//       justifyContent: 'center',
-//     },
-//   });
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+    linearGradient: {
+      flex: 1,
+      paddingLeft: 15,
+      paddingRight: 15,
+      borderRadius: 5
+    },
+    card: {
+      backgroundColor: '#69DDFF',
+    }
+  });
--- components/screens/main/musicscreen.js
+++ components/screens/main/musicscreen.js
@@ -1,118 +1,23 @@
 import React, { Component } from "react";
 import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
-import Search from "../../SearchBar";
-import Listing from "../../spotify_api/Listing";
-import spotify_search from "../../spotify_api/spotify_search";
-import spotify_token from "../../spotify_api/spotify_token";
-
-const pageLimit = 10;
+import { SearchBar } from "react-native-elements";
 
 class Music extends Component {
-  // constructor(props) {
-  //   super(props);
 
-  //   this.state = {
-  //     tracks: [],
-  //     offset: 0,
-  //     query: "",
-  //     isFetching: false,
-  //     isEmpty: false,
-  //     spotify_token: null,
-  //     isTokenFetching: false
-  //   };
-  // }
+  state={
+    search:'',
+  };
 
-  // async componentDidMount() {
-  //   await this.refreshToken();
-  //   await this.loadNextPage();
-  // }
-
-  // async componentWillMount() {
-  //   this.refreshToken();
-  //   this.loadNextPage();
-  // }
-
-  // handleSearch(text) {
-  //   this.setState(
-  //     {
-  //       isEmpty: false,
-  //       query: text,
-  //       offset: 0,
-  //       tracks: []
-  //     },
-  //     () => {
-  //       this.nextPage();
-  //     }
-  //   );
-  // }
-
-  // async loadNextPage() {
-  //   const {
-  //     tracks,
-  //     offset,
-  //     query,
-  //     spotify_token,
-  //     isFetching,
-  //     isEmpty
-  //   } = this.state;
-
-  //   if(isFetching || isEmpty) {
-  //     return;
-  //   }
-
-  //   this.setState({isFetching: true});
-
-  //   const newTrack = await spotify_search({
-  //     offset: offset,
-  //     limit: pageLimit,
-  //     q: query,
-  //     spotify_token
-  //   });
-
-  //   if(newTrack.length === 0) {
-  //     console.log("no songs found");
-  //     this.setState({isEmpty: true});
-  //   }
-
-  //   this.setState({
-  //     isFetching: false,
-  //     tracks: [...tracks, ...newTrack],
-  //     offset: offset + pageLimit
-  //   });
-  // }
-
-  // async refreshToken() {
-  //   this.setState({
-  //     isTokenFetching: true
-  //   });
-
-  //   const newToken = await spotify_token();
-  //   this.setState({
-  //     spotify_token: newToken,
-  //     isTokenFetching: false
-  //   });
-  // }
-
-  // async handleEndReached() {
-  //   await this.loadNextPage();
-  // }
-
+  updateSearch = (search) => {
+    this.setState({search});
+  };
+  
     render() {
-      // const {query, tracks, isFetching} = this.state;
+      const {search} = this.state;
 
         return (
             <View style={styles.container}>
-              {/* <Search 
-                onChange={text => this.handleSearch(text)} 
-                value={query}
-              /> */}
-              {/* {
-                isFetching && tracks.length === 0 ? (ActivityIndicator) : 
-                (<Listing 
-                  items={tracks}
-                  onEndReached={() => this.handleEndReached()}
-                />)
-              } */}
+              <SearchBar placeholder="Search for a song" onChangeText={this.updateSearch} value={search} />
               <Text>Music Streaming!</Text>
             </View>
         );
@@ -125,7 +30,5 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
     },
   });
